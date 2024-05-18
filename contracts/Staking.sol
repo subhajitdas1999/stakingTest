4022360b123dd11dbc263d4245eafb041389dc10
subhajitdas1999 <sd651846@gmail.com>
2024-05-19 01:23:04 +0530

    added events,implemented unstake,claimRewards and all the getter function

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -3,10 +3,11 @@ pragma solidity 0.8.21;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract Staking is Ownable {
-    uint public poolId;
-    uint public totalStakers;
-    uint public totalActiveStakers;
+    uint256 public poolId;
+    uint256 public totalStakers;
+    uint256 public totalActiveStakers;
     IERC20 public stakingTokenAddress;
 
     struct Pool {
@@ -14,28 +15,55 @@ contract Staking is Ownable {
         uint256 poolDuration;
         uint256 lockInDuration;
         uint256 totalTokenStaked;
+        uint256 totalRewardDistributed;
     }
 
     struct Stake {
-        uint256 stakeAmount;
-        uint256 stateAt;
+        uint256 stakingAmount;
+        uint256 stakedAt;
+        uint256 rewardAccumulated;
+        uint256 lastClaimedAt;
         bool isActive;
     }
     //poolId -> pool
-    mapping(uint => Pool) public pools;
+    mapping(uint256 => Pool) public pools;
     //walletAddress -> poolId -> Stake
-    mapping(address => mapping(uint => Stake)) public stakes;
+    mapping(address => mapping(uint256 => Stake)) public stakes;
+
+    //poolId => address[]
+    mapping(uint256 => address[]) public stakers;
 
     //Errors
     error InvalidZeroAmount();
     error InvalidPoolId();
+    error InvalidStaking();
+    error PoolTimeOver();
+
+    //modifiers
+    modifier validStaking(address _staker, uint256 _poolId) {
+        if (_poolId == 0 || _poolId > poolId) revert InvalidStaking();
+        if (!stakes[_staker][_poolId].isActive) revert InvalidStaking();
+        _;
+    }
 
     //events
     event PoolCreated(
-        uint poolId,
-        uint poolDistributionAmount,
-        uint poolDuration,
-        uint lockInDuration
+        uint256 poolId,
+        uint256 poolDistributionAmount,
+        uint256 poolDuration,
+        uint256 lockInDuration
+    );
+    event Staked(address indexed staker, uint256 poolId, uint256 stakingAmount);
+
+    event UnStaked(
+        address indexed staker,
+        uint256 poolId,
+        uint256 unStakingAmount
+    );
+    event RewardClaimed(
+        address indexed staker,
+        uint256 poolId,
+        uint256 rewardAmount
     );
 
     constructor(IERC20 _stakingTokenAddress) Ownable(msg.sender) {
@@ -43,16 +71,16 @@ contract Staking is Ownable {
     }
 
     function createPool(
-        uint _poolDistributionAmount,
-        uint _poolDuration,
-        uint _lockInDuration
-    ) external onlyOwner {
+        uint256 _poolDistributionAmount,
+        uint256 _poolDuration,
+        uint256 _lockInDuration
+    ) external {
         if (_poolDistributionAmount == 0) revert InvalidZeroAmount();
         if (_poolDuration == 0) revert InvalidZeroAmount();
         if (_lockInDuration == 0) revert InvalidZeroAmount();
 
         require(
-            stakingTokenAddress.allowance(owner(), address(this)) ==
+            stakingTokenAddress.allowance(owner(), address(this)) >=
                 _poolDistributionAmount,
             "Token amount is not approved"
         );
@@ -64,23 +92,25 @@ contract Staking is Ownable {
         poolId++;
         pools[poolId] = Pool(
             _poolDistributionAmount,
-            _poolDuration,
+            block.timestamp + _poolDuration,
             _lockInDuration,
+            0,
             0
         );
         emit PoolCreated(
             poolId,
             _poolDistributionAmount,
-            _poolDuration,
+            block.timestamp + _poolDuration,
             _lockInDuration
         );
     }
 
-    function stake(uint _stakingAmount, uint _poolId) external {
+    function stake(uint256 _stakingAmount, uint256 _poolId) external {
         if (_stakingAmount == 0) revert InvalidZeroAmount();
         if (_poolId == 0 || _poolId > poolId) revert InvalidPoolId();
+        if (pools[poolId].poolDuration < block.timestamp) revert PoolTimeOver();
         require(
-            stakingTokenAddress.allowance(_msgSender(), address(this)) ==
+            stakingTokenAddress.allowance(_msgSender(), address(this)) >=
                 _stakingAmount,
             "Token amount is not approved"
         );
@@ -93,20 +123,145 @@ contract Staking is Ownable {
         //if staking already exist add the token amount, else create a new staking
         if (stakes[_msgSender()][_poolId].isActive == true) {
             stakes[_msgSender()][_poolId].stakingAmount += _stakingAmount;
-            stakes[_msgSender()][_poolId].stateAt = block.timestamp;
+            stakes[_msgSender()][_poolId].stakedAt = block.timestamp;
+            stakes[_msgSender()][_poolId].lastClaimedAt = block.timestamp;
         } else {
+            //if new user to the platform
+            if (stakes[_msgSender()][_poolId].stakedAt == 0) {
+                stakers[_poolId].push(_msgSender());
+            }
+
             stakes[_msgSender()][_poolId] = Stake(
                 _stakingAmount,
+                block.timestamp,
+                0,
                 block.timestamp,
                 true
             );
             totalStakers += 1;
             totalActiveStakers += 1;
         }
+
         pools[poolId].totalTokenStaked += _stakingAmount;
+        emit Staked(_msgSender(), _poolId, _stakingAmount);
     }
 
-    function unStake(uint _poolId) external {
+    function unStake(
+        uint256 _poolId
+    ) external validStaking(_msgSender(), _poolId) {
+        Stake storage userStake = stakes[_msgSender()][_poolId];
+        Pool storage pool = pools[_poolId];
+        require(
+            block.timestamp >= userStake.stakedAt + pool.lockInDuration,
+            "Lock-in period not over"
+        );
+
+        _claim(_msgSender(), _poolId);
+
+        uint256 amountToUnStake = userStake.stakingAmount;
+        userStake.stakingAmount = 0;
+        userStake.isActive = false;
+        pool.totalTokenStaked -= amountToUnStake;
+        totalActiveStakers -= 1;
+        stakingTokenAddress.transfer(_msgSender(), amountToUnStake);
+
+        emit UnStaked(_msgSender(), _poolId, amountToUnStake);
+    }
+
+    function claimRewards(
+        uint256 _poolId
+    ) external validStaking(_msgSender(), _poolId) {
+        _claim(_msgSender(), _poolId);
+    }
+
+    function calculateReward(
+        address _staker,
+        uint256 _poolId
+    ) public view returns (uint256) {
+        Pool storage pool = pools[_poolId];
+        Stake storage userStake = stakes[_staker][_poolId];
+
+        if (pool.totalTokenStaked == 0) {
+            return 0;
+        }
+
+        uint256 timeSinceLastClaim = block.timestamp - userStake.lastClaimedAt;
+        uint256 daysSinceLastClaim = timeSinceLastClaim / 10 seconds;
+
+        uint256 dailyDistribution = pool.poolDistributionAmount /
+            pool.poolDuration;
+        uint256 perTokenShare = (dailyDistribution * 1e18) /
+            pool.totalTokenStaked;
+        uint256 reward = (perTokenShare *
+            userStake.stakingAmount *
+            daysSinceLastClaim) / 1e18;
+
+        return reward;
+    }
+
+    function _claim(address _staker, uint256 _poolId) internal {
+        Stake storage userStake = stakes[_staker][_poolId];
+        Pool storage pool = pools[_poolId];
+        uint256 reward = calculateReward(_staker, _poolId);
+        userStake.lastClaimedAt = block.timestamp;
+        userStake.rewardAccumulated += reward;
+        pool.totalRewardDistributed += reward;
+        stakingTokenAddress.transfer(_staker, reward);
+        emit RewardClaimed(_staker, _poolId, reward);
+    }
+
+    function getCurrentHourlyRewardEmission(
+        uint256 _poolId
+    ) external view returns (uint256) {
         if (_poolId == 0 || _poolId > poolId) revert InvalidPoolId();
+        Pool memory pool = pools[_poolId];
+        uint256 dailyDistribution = pool.poolDistributionAmount /
+            pool.poolDuration;
+        return dailyDistribution / 24;
+    }
+
+    function getTotalPoolAmountLeft(
+        uint256 _poolId
+    ) external view returns (uint256) {
+        if (_poolId == 0 || _poolId > poolId) revert InvalidPoolId();
+        Pool memory pool = pools[_poolId];
+        return pool.poolDistributionAmount - pool.totalRewardDistributed;
+    }
+
+    function getTokenStakedInPool(
+        uint256 _poolId
+    ) external view returns (uint256) {
+        return pools[_poolId].totalTokenStaked;
+    }
+
+    function getAllStakersList(
+        uint256 _poolId,
+        bool _isActive
+    ) external view returns (address[] memory) {
+        if (_poolId == 0 || _poolId > poolId) revert InvalidPoolId();
+
+        if (_isActive) {
+            uint256 count = 0;
+
+            for (uint256 i = 0; i < stakers[_poolId].length; i++) {
+                if (stakes[stakers[_poolId][i]][_poolId].isActive) {
+                    count++;
+                }
+            }
+
+            address[] memory allStakers = new address[](count);
+            uint256 idx = 0;
+
+            for (uint256 i = 0; i < stakers[_poolId].length; i++) {
+                if (stakes[stakers[_poolId][i]][_poolId].isActive) {
+                    allStakers[idx] = stakers[_poolId][i];
+                    idx++;
+                }
+            }
+
+            return allStakers;
+        } else {
+            return stakers[_poolId];
+        }
     }
 }
